f6c481402fbf0523ca89ec1a9765c97b09c5ba4c
filippoanselmi <116039968+filippoanselmi@users.noreply.github.com>
2022-12-20 21:20:19 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
+float f(float y){
+   return (((y*y)*cos(y))+1);
+}
+
 int main() {
+   float a = 0, b = 0, x = 0, err = 0;
+   cout << "inserire estremi" << endl;
+   cin >> a >> b;
+   
+   while ((f(a)*f(b)) >= 0){
+      cout << "inserire estremi" << endl;
+      cin >> a >> b;
+   }
+   
+   do {
+      x = ((a+b)/2);
+      if (f(x) == 0){
+      } else {
+         if ((f(a)*f(x)) < 0){
+            b = x;
+         } else if ((f(a)*f(x)) > 0){
+            a = x;
+         }
+         err = (abs((b-a)/2));
+      }
+   } while (err >= (1/(pow(M_E, 6))));
+   
+   x = x*1000;
+   float risultato1 = int (x);
+   float risultato2 = risultato1/1000;
+   cout << risultato2;
+   
    return 0;
 }
